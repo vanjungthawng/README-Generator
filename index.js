a64deac8c42342cb08fe8c84f5eaef78afaefe66
AJUNG <cung@Vans-MacBook-Pro.local>
2021-10-21 22:43:31 +1100

    Implement genReadMe function

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+// Generate README file
+const genReadMe = ({
+  username,
+  email,
+  title,
+  description,
+  license,
+  installation,
+  test,
+  usage,
+  contribution,
+}) => {
+  let licenseBadge;
+  if (license === "MIT") {
+    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Apache 2.0") {
+    licenseBadge = `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "GPL") {
+    licenseBadge = `[![License: GPL](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "ISC") {
+    licenseBadge = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+  }
+  return `
+  # ${title}
+  ${licenseBadge}
+  
+  ## Description
+  ${description}
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [Contact](#contact)
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usage}
+  
+  ## License
+  This project is licensed under the ${license} license.
+  
+  ## Contribution
+  ${contribution}
+  
+  ## Test
+  ${test}
+  
+  ## Contact
+  Email: ${email}
+  
+  Github: [Click Here!](https://github.com/${username})
+  `;
+};
+
 // Questions
 inquirer.prompt([
   {

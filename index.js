9d11642b28fe55166182cd8b65b6bcf0c3327c4b
AJUNG <cung@Vans-MacBook-Pro.local>
2021-10-21 22:47:34 +1100

    add callback function

--- index.js
+++ index.js
@@ -62,51 +62,62 @@ const genReadMe = ({
 };
 
 // Questions
-inquirer.prompt([
-  {
-    type: "input",
-    name: "username",
-    message: "What is your GitHub username?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-  },
-  {
-    type: "input",
-    name: "title",
-    message: "What's your project's name?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Write a short description of your project: ",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "What flavor is your license?",
-    choices: ["MIT", "Apache 2.0", "GPL", "ISC"],
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What command should be run to install dependencies?",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "What command should be run to run tests?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What does the user need to know about using the repo?",
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message: "What does the user need to know about contributing to the repo?",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What's your project's name?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Write a short description of your project: ",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What flavor is your license?",
+      choices: ["MIT", "Apache 2.0", "GPL", "ISC"],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message:
+        "What does the user need to know about contributing to the repo?",
+    },
+  ])
+  .then((answers) => {
+    const readMeFormat = genReadMe(answers);
+
+    fs.writeFile("READMee.md", readMeFormat, (err) =>
+      err
+        ? console.log(err)
+        : console.log("Successfully created the README file!")
+    );
+  });
